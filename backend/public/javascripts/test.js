a48c5e9bdc72a04ed8ff5e25b1772f4fabd27a70
Zakiah Schaefle <zschaefle1@gmail.com>
2019-12-11 23:09:24 -0800

    whoops
    wrong assignment variable

--- backend/public/javascripts/test.js
+++ backend/public/javascripts/test.js
@@ -10,7 +10,7 @@ function getTestData() {
 function login(){
     var req = new XMLHttpRequest();
     req.open('POST', '/auth');
-    req.body = {username:'hello', password = 'test!'};
+    req.body = {username:'hello', password: 'test!'};
     req.addEventListener('load', function(event) {
         console.log(event.target.response);
     });
